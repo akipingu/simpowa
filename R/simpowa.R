5f99d83e9545a3ef9dff4933b457332b33d07e7d
Andrea Kipingu <andreakipingu@gmail.com>
2025-11-19 14:45:25 +0300

    Renamed package from powergene to simpowa

--- R/simpowa.R
+++ R/simpowa.R
@@ -1,7 +1,7 @@
-#' powergene: A Generalizable Simulation-Based Power Analysis R Package for Semi-Field Vector Control Trials
+#' simpowa: A Generalizable Simulation-Based Power Analysis R Package for Semi-Field Vector Control Trials
 #'
 #' The methods for a simulation-based power analysis tailored to short- or long-term semi-field vector control trials testing single or combined interventions.
-#' The \code{powergene} package supports random effects modeling, empirical p-value extraction, and
+#' The \code{simpowa} package supports random effects modeling, empirical p-value extraction, and
 #' parallelized workflows. It is designed to guide experimental design and improve statistical rigor
 #' in entomological research.
 "_PACKAGE"
--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(powergene)
+library(simpowa)
 
-test_check("powergene")
+test_check("simpowa")
